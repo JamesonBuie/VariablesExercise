330497f4c0cae7fa22753cbe1a6580ce9e54d04d
Jameson Buie <jamesonbuie@gmail.com>
2022-02-14 13:54:33 -0600

    Completed Variables Exercise

---
+++ script.js
@@ -0,0 +1,17 @@
+// const
+
+let num = 20;
+num = 15;
+num += 7;
+num--;
+num = "This is not a number";
+let blank;
+console.log(blank);
+blank = null;
+console.log(blank);
+
+//  BONUS  
+const [quarter, dime, nickel, penny] = [.25, .10, .05, .01]
+console.log(quarter, dime, nickel, penny);
+let letters = "qwertyuiop"
+console.log(letters[letters.length-1]);
